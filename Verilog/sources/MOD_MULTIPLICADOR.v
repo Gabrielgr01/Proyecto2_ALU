8b339bf37a00de697bff6ede8c18e9398b848b7a
Gabriel González Rodríguez <gabrielgr01@hotmail.com>
2021-10-10 16:25:59 -0600

    Update MOD_MULTIPLICADOR.v

--- Verilog/sources/MOD_MULTIPLICADOR.v
+++ Verilog/sources/MOD_MULTIPLICADOR.v
@@ -26,25 +26,28 @@ module MOD_MULTIPLICADOR (
     output OF_MUL
     );
     
+    // Conversion complemento a 2 para A
     wire [5:0] A_comp2, A_conv;
     MOD_Conversor_Comp2 conversionA_conv(A, A_comp2);
     Mux_2a1 conversion_A_mux (A[5],A, A_comp2, A_conv);
     
+    // Conversion complemento a 2 para B
     wire [5:0] B_comp2, B_conv;
     MOD_Conversor_Comp2 conversionB_conv(B, B_comp2);
     Mux_2a1 conversion_B_mux (B[5], B, B_comp2, B_conv);
     
+    // Multiplicacion positiva
     wire [9:0] mult_pos_completa;
     wire [5:0] mult_pos_incompleta;
     assign mult_pos_completa = A_conv * B_conv;
     assign mult_pos_incompleta = mult_pos_completa[5:0];
     
+    // Conversion complemento a 2 para el resultado de la multiplicación positiva
     wire [5:0] mult_comp2; 
     wire SEL_conv_mult;
     assign SEL_conv_mult = A[5] ^ B[5];
     MOD_Conversor_Comp2 conversionMult_conv (mult_pos_incompleta, mult_comp2);
-    
-    Mux_2a1 conversionMult_mux (SEL_conv_mult, mult_pos_incompleta, mult_comp2, M);
+    Mux_2a1 conversionMult_mux (SEL_conv_mult, mult_pos_incompleta, mult_comp2, M); // Resultado: M
     
     // Bandera Overflow (OF)
     wire OF_or;
